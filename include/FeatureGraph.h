e5206a3a2d8008512bfe51749e95c8c3fceb0b8b
mwhicks2 <mwhicks2@ncsu.edu>
2024-02-16 12:00:51 -0500

    Changed edge weight to param state

--- include/FeatureGraph.h
+++ include/FeatureGraph.h
@@ -7,19 +7,23 @@
 
 namespace grapph {
 
-    template <typename T>
+    template <typename V, typename E>
     class FeatureGraph : public Graph {
 
     private:
 
-        std::map<vertex_t, T> vertex_state;
-        std::map<edge_t, long int> edge_weight;
+        std::map<vertex_t, V> vertex_state;
+        std::map<edge_t, E> edge_state;
 
-        long int default_weight = 1;
-        T (*state_constructor_function)(vertex_t) = defaultStateConstructor;
+        V (*vertex_auto_state)(vertex_t) = defaultVertexState;
+        E (*edge_auto_state)(edge_t) = defaultEdgeState;
 
-        static T defaultStateConstructor(vertex_t) {
-            throw std::logic_error("No state constructor defined");
+        static V defaultVertexState(vertex_t) {
+            throw std::logic_error("No vertex auto state defined");
+        }
+
+        static E defaultEdgeState(edge_t) {
+            throw std::logic_error("No edge auto state defined");
         }
 
     protected:
@@ -30,38 +34,34 @@ namespace grapph {
     public:
 
         FeatureGraph() = default;
-        FeatureGraph(std::vector<std::pair<vertex_t, T>>, std::set<edge_t>);
-        FeatureGraph(std::vector<std::pair<vertex_t, T>>, std::vector<std::pair<edge_t, long int>>);
-        FeatureGraph(std::set<vertex_t>, std::set<edge_t>, T (*)(vertex_t), long int);
-        FeatureGraph(std::set<vertex_t>, std::vector<std::pair<edge_t, long int>>, T (*)(vertex_t));
-        FeatureGraph(std::vector<std::pair<vertex_t, T>>, std::set<edge_t>, long int);
+        FeatureGraph(std::vector<std::pair<vertex_t, V>>, std::vector<std::pair<edge_t, E>>);
 
         vertex_t addVertex() override;
         vertex_t addVertex(vertex_t) override;
-        vertex_t addVertex(T);
-        vertex_t addVertex(vertex_t, T);
+        vertex_t addVertex(V);
+        vertex_t addVertex(vertex_t, V);
 
-        void updateVertex(vertex_t, T);
+        void updateVertex(vertex_t, V);
 
         void removeVertex(vertex_t) override;
 
         edge_t addEdge(vertex_t, vertex_t) override;
         edge_t addEdge(edge_t) override;
-        edge_t addEdge(vertex_t, vertex_t, long int);
-        edge_t addEdge(edge_t, long int);
+        edge_t addEdge(vertex_t, vertex_t, E);
+        edge_t addEdge(edge_t, E);
 
-        void updateEdge(edge_t, long int);
+        void updateEdge(edge_t, E);
 
         void removeEdge(edge_t) override;
 
-        T getVertexState(vertex_t vertex) { Graph::validate(vertex); return vertex_state[vertex]; }
-        std::map<vertex_t, T> getVertexStates() { return vertex_state; }
+        V getVertexState(vertex_t vertex) { Graph::validate(vertex); return vertex_state[vertex]; }
+        std::map<vertex_t, V> getVertexStates() { return vertex_state; }
 
-        long int getEdgeWeight(edge_t edge) { validate(edge); return edge_weight[edge]; }
-        std::map<edge_t, long int> getEdgeWeights() { return edge_weight; }
+        E getEdgeState(edge_t edge) { validate(edge); return edge_state[edge]; }
+        std::map<edge_t, E> getEdgeWeights() { return edge_state; }
 
-        void setStateConstructor(T(*func)(vertex_t)) { state_constructor_function = func; }
-        void setDefaultWeight(long int new_default_weight) { this->default_weight = new_default_weight; }
+        void setVertexAutoState(V(*func)(vertex_t)) { vertex_auto_state = func; }
+        void setEdgeAutoState(E(*func)(edge_t)) { edge_auto_state = func; }
 
     };
 
